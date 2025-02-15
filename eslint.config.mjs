96f3efc392ac521b0dd4226de7afc96f7fa4ca7f
Tahara Kazuki <>
2025-02-15 10:18:31 +0900

    Add Tailwind CSS utilities, ESLint plugin, and related dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import importPlugin from 'eslint-plugin-import';
 import prettier from 'eslint-plugin-prettier';
+import tailwindcssPlugin from 'eslint-plugin-tailwindcss';
 import unusedImports from 'eslint-plugin-unused-imports';
 
 const eslintConfig = [
@@ -19,10 +20,12 @@ const eslintConfig = [
       import: importPlugin,
       'unused-imports': unusedImports,
       '@typescript-eslint': tseslint,
+      tailwindcss: tailwindcssPlugin,
     },
     rules: {
       'prettier/prettier': 'error',
       'no-unused-vars': 'off',
+      'tailwindcss/no-custom-classname': [2],
       'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
@@ -51,6 +54,10 @@ const eslintConfig = [
           },
         },
       ],
+      'tailwindcss/classnames-order': 'error',
+      'tailwindcss/enforces-negative-arbitrary-values': 'error',
+      'tailwindcss/enforces-shorthand': 'error',
+      'tailwindcss/no-contradicting-classname': 'error',
     },
   },
 ];
