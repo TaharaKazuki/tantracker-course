6609f579494d293689e8a0656a2abf6409279fc7
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-02-18 16:33:48 +0900

    Add date picker and complete transaction form UI with Radix UI Popover

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -1,6 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 
+import { Button } from './ui/button';
+import { Calendar } from './ui/calendar';
 import {
   Form,
   FormControl,
@@ -9,6 +13,8 @@ import {
   FormLabel,
   FormMessage,
 } from './ui/form';
+import { Input } from './ui/input';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 import {
   Select,
   SelectContent,
@@ -17,6 +23,7 @@ import {
   SelectValue,
 } from './ui/select';
 
+import { cn } from '@/lib/utils';
 import {
   transactionFormSchema,
   TransactionSchemaType,
@@ -38,6 +45,7 @@ export function TransactionForm() {
     <Form {...form}>
       <form>
         <fieldset className="grid grid-cols-2 gap-x-2 gap-y-5">
+          {/* Transaction Type */}
           <FormField
             control={form.control}
             name="transactionType"
@@ -61,6 +69,113 @@ export function TransactionForm() {
               );
             }}
           />
+
+          {/* Category */}
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Transaction Type</FormLabel>
+                  <FormControl>
+                    <Select
+                      value={field.value.toString()}
+                      onValueChange={field.onChange}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Category" />
+                      </SelectTrigger>
+                      <SelectContent></SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
+          {/* Transaction Date */}
+          <FormField
+            control={form.control}
+            name="transactionDate"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Transaction Date</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'w-full justify-start text-left font-normal',
+                            !field.value && 'text-muted-foreground'
+                          )}
+                        >
+                          <CalendarIcon className="mr-2 size-4" />
+                          {field.value ? (
+                            format(field.value, 'PPP')
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                          disabled={{
+                            after: new Date(),
+                          }}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
+          {/* amount */}
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="number" step={0.01} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+        </fieldset>
+        <fieldset className="mt-5 flex flex-col gap-5">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <Button type="submit">
+            {form.formState.isSubmitting ? 'Submitting...' : 'Submit'}
+          </Button>
         </fieldset>
       </form>
     </Form>
